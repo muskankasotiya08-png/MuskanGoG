04b2d9c072ec8a4d70f51f3354f8209a1c2a0e82
muskankasotiya08-png <muskankasotiya08@gmail.com>
2025-12-05 14:50:08 +0530

    Create TokenPath contract with ERC20 features
    Implement ERC20 token contract with basic functionality.

--- contracts/TokenPath.sol
+++ contracts/TokenPath.sol
@@ -1,12 +1,30 @@
-End
-End
-End
-End
-End
-End
-End
-End
-// 
-// 
-End
-// 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract TokenPath {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _initialSupply) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = _initialSupply;
+        balanceOf[msg.sender] = _initialSupply;
+        emit Transfer(address(0), msg.sender, _initialSupply);
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+}
